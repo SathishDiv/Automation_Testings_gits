d7d5886805516f29d2a560b842b909de980e2330
sathishsuriya <sathishsuriya@gmail.com>
2021-11-27 09:27:56 +0530

    One Version

---
+++ SI_SampleProjects/src/FirstOne.java
@@ -0,0 +1,18 @@
+
+public class FirstOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("DataScience");
+		
+		System.out.println("Ai");
+		
+		System.out.println("Tosca");
+		
+		System.out.println("Uft");
+		
+		System.out.println("oracle");
+	}
+
+}
